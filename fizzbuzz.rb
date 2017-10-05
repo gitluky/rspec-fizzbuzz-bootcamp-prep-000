52f98a8ccffc5d30fedfffa34119c33b9da38555
gitluky <jluk87@gmail.com>
2017-10-05 21:43:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
   if num%3==0 && num%5==0
-    return 'fizzbuzz'
+    return 'Fizzbuzz'
   elsif num%3==0 && num%5!=0
-    return 'fizz'
+    return 'Fizz'
   elsif num%3!=0 && num%5==0
-    return 'buzz'
+    return 'Buzz'
   end
 end
